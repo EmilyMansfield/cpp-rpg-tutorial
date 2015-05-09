f1c684f8358e769aaedc3f087aa55145b3ccb486
Daniel Mansfield <daniel.mansfield99@gmail.com>
2015-05-09 15:20:33 +0100

    Commented EntityManager

--- src/entity_manager.hpp
+++ src/entity_manager.hpp
@@ -22,17 +22,23 @@ class EntityManager
 	std::map<std::string, Area> dataArea;
 	std::map<std::string, Door> dataDoor;
 
+	// Read the JSON file given by filename and interpret it according
+	// to the type T, before storing all entities read in the data map
 	template<typename T>
 	void loadJson(std::string filename, std::map<std::string, T>& data);
 
 	public:
 
+	// Load the JSON file and determine which map to save the data to
+	// according to the type T
 	template<typename T>
 	void loadJson(std::string filename);
 
+	// Return the entity given by id
 	template<typename T>
 	T* getEntity(std::string id);
 
+	// Constructor
 	EntityManager();
 
 };
